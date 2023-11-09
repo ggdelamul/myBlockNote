46536e7bd0888ba29a74a5cc47cda929e21a3daa
jeremy <projeremylegendre@gmail.com>
2023-11-09 16:28:20 +0100

    added style

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector("button");
 const textarea = document.querySelector("textarea");
+textarea.value = window.localStorage.getItem("note");
 btn.addEventListener("click", () => {
   console.log("click");
   window.localStorage.setItem("note", textarea.value);
