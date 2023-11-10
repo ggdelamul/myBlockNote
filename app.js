30efb3cc96302f53b1b899519908b7439b72e4f0
jeremy <projeremylegendre@gmail.com>
2023-11-10 16:18:30 +0100

    added new Note div

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+/* apparition de la fenetre de creation de nouvelle note*/
+const newLink = document.querySelector(".newLink");
+const newNoteInputContainer = document.querySelector(".newNoteInput-container");
+console.log(newNoteInputContainer);
+newLink.addEventListener("click", () => {
+  newNoteInputContainer.style.display = "flex";
+});
+
 const btnSave = document.querySelector("#save");
 const btnDelete = document.querySelector("#delete");
 const textarea = document.querySelector("textarea");
