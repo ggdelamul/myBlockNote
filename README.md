1e8554c23e687336b6e2c6c24ccd14a26c6ac4ff
jeremy <projeremylegendre@gmail.com>
2023-11-10 14:44:03 +0100

    End of V1

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Mon petite block note est un extension pour chrome permettant d'enregistrer du texte pour agder des notes pour plus tard
 
 Elle utilise le local storage du navigateur pour sauvegader les données
+
+## version 1
+
+La version 1 de l'extension permet d'enregistrer le contenu du textarea et d'effacer le contenu
+branch v1;
--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const btn = document.querySelector("button");
+const btnSave = document.querySelector("#save");
+const btnDelete = document.querySelector("#delete");
 const textarea = document.querySelector("textarea");
 textarea.value = window.localStorage.getItem("note");
-btn.addEventListener("click", () => {
+btnSave.addEventListener("click", () => {
   console.log("click");
   window.localStorage.setItem("note", textarea.value);
+});
+btnDelete.addEventListener("click", () => {
+  window.localStorage.removeItem("note");
+  textarea.value = "";
 });
